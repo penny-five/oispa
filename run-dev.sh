050c37f7560956b6ecf6630487943a7d256f4e42
Joonas Lehtonen <lehton83@gmail.com>
2017-02-18 11:36:22 +0200

    add volume for postgres

--- run-dev.sh
+++ run-dev.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 docker-compose down \
-&& docker-compose -f docker-compose.yml -f docker-compose.dev.yml up --build -d \
-&& (cd frontend && yarn run dev) \
-&& echo "Oispa dev environment up and running. Run \"docker-compose logs -f\" to see logs."
+&& docker-compose -f docker-compose.yml -f docker-compose.dev.yml up --force-recreate -d \
+&& echo "Oispa dev environment up and running." \
+&& echo "Run \"docker-compose logs -f\" to see logs." \
+&& echo "Run \"cd frontend && yarn run dev\" to start webpack dev server."
