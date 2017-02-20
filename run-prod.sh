fb63b0b0863b061de1f2e720f8208d8e5f9f5298
Joonas Lehtonen <lehton83@gmail.com>
2017-02-20 20:48:28 +0200

    rename prod run script

--- run-prod.sh
+++ run-prod.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 docker-compose down \
+&& (cd frontend && yarn install && yarn build) \
 && docker-compose build \
 && docker-compose -f docker-compose.yml up --build -d \
 && echo "Oispa up and running. Run \"docker-compose logs -f\" to see logs."
