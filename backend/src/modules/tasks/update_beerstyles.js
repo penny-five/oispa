0b8d99cf6ef166af4f425d7bd6d3c1352d3f62ab
Joonas Lehtonen <lehton83@gmail.com>
2017-03-08 20:13:08 +0200

    fix beer style name encoding

--- backend/src/modules/tasks/update_beerstyles.js
+++ backend/src/modules/tasks/update_beerstyles.js
@@ -1,5 +1,6 @@
 const _ = require('lodash');
 const scrapeIt = require('scrape-it');
+const he = require('he');
 
 const knex = require('../../knex');
 const upsert = require('../../utils/knex').upsert;
@@ -28,17 +29,14 @@ const SCRAPE_SPEC = {
 module.exports = async logger => {
   logger.info('Updating beer styles...');
 
-  const data = await scrapeIt({
-    url: SCRAPE_URL,
-    encoding: 'UTF-8'
-  }, SCRAPE_SPEC);
+  const data = await scrapeIt(SCRAPE_URL, SCRAPE_SPEC);
 
   /* First item in <select> element is an empty option and needs to be dropped */
   let items = _.drop(data.items);
 
   items = items.map(item => ({
     id: parseInt(item.id, 10),
-    name: item.name,
+    name: he.decode(item.name),
     category: findCategoryForBeerStyle(item)
   }));
 
